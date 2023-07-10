9fa00a4cbcad9ce3d53fa5989d5d9dbfabf59d48
SHARATH <111446084+SHARATH-20-07-2002@users.noreply.github.com>
2023-07-10 20:26:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("keydown", function(event) {
+    if(event.code === "Enter") {
+        search();
+    }
+});
+
+function search() {
+    const input = searchInput.value;
+
+    window.location.href = "http://192.168.29.213:5500/facebook/index2.html"
+}
